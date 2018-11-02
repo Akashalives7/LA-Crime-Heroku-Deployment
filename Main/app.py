779933bc498f23d16a87d8bf2bf111159b3ae099
Akashalives7 <35281785+Akashalives7@users.noreply.github.com>
2018-11-01 17:06:43 -0700

    app.py with rest commented out

--- Main/app.py
+++ Main/app.py
@@ -13,29 +13,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/choropleth")
-def home():
-    return render_template("VisJH1.html")
+# @app.route("/choropleth")
+# def home():
+#     return render_template("VisJH1.html")
 
-@app.route("/choropleth")
-def home():
-    return render_template("VisJH2.html")
+# @app.route("/choropleth")
+# def home():
+#     return render_template("VisJH2.html")
 
-@app.route("/scatter")
-def home():
-    return render_template("VisSS1.html")
+# @app.route("/scatter")
+# def home():
+#     return render_template("VisSS1.html")
 
-@app.route("/scatter")
-def home():
-    return render_template("VisSS2.html")
+# @app.route("/scatter")
+# def home():
+#     return render_template("VisSS2.html")
 
-@app.route("/chartjs")
-def home():
-    return render_template("VisBK1.html")
+# @app.route("/chartjs")
+# def home():
+#     return render_template("VisBK1.html")
 
-@app.route("/chartjs")
-def home():
-    return render_template("VisBK2.html")
+# @app.route("/chartjs")
+# def home():
+#     return render_template("VisBK2.html")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8000)
